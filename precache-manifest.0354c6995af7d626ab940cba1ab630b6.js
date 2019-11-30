ba61060e52e16a9a46334f4e3633b095b13a94ac
Nahuel Verdugo Revigliono <nahuelvr@gmail.com>
2019-11-30 01:26:34 +0100

    Updates

---
+++ precache-manifest.0354c6995af7d626ab940cba1ab630b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8552c3df6c140ca2f8c7299accca4cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b2cc4c5b789c21e2825",
+    "url": "/static/css/main.82e3b772.chunk.css"
+  },
+  {
+    "revision": "ccd577c8b8e7ecc9f605",
+    "url": "/static/js/2.3b3e0432.chunk.js"
+  },
+  {
+    "revision": "0b2cc4c5b789c21e2825",
+    "url": "/static/js/main.cd346110.chunk.js"
+  },
+  {
+    "revision": "ca12f755a6f12f041e95",
+    "url": "/static/js/runtime-main.98a937aa.js"
+  }
+]);
